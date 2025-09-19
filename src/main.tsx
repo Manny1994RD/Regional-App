8c62e13f4af628d962c2fe8d49a9bf0765b6b278
Manny1994RD <m.rosario1234@gmail.com>
2025-09-19 00:04:18 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+window.addEventListener("error", (e) => {
+  console.error("[window.error]", e.message, e.error?.stack || e.error)
+});
+window.addEventListener("unhandledrejection", (e) => {
+  console.error("[unhandledrejection]", e.reason);
+});
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
