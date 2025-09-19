865442b26ac0f564c7c2454a16ed271f52bf2607
Manny1994RD <m.rosario1234@gmail.com>
2025-09-18 23:40:56 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // Use '/' on Vercel, '/Regional-App/' on GitHub Pages
 const isVercel = !!process.env.VERCEL
+
 export default defineConfig({
   base: isVercel ? '/' : '/Regional-App/',
   plugins: [react()],
